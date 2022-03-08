8d9febc76265ff5068716fa71bec1ae412642f55
leejeonghan <wjdgks300@naver.com>
2022-03-08 23:17:15 +0900

    backjoon

--- 백준/15684번 사다리 - 진행중,,.cpp
+++ 백준/15684번 사다리 - 진행중,,.cpp
@@ -1,40 +1,69 @@
 #include<bits/stdc++.h>	
 using namespace std;
 
-int n, m, h;
-int ladder[31][11];
-int dd[2] = {1,-1};
+// 왜틀렸는지 모르겠음,,,,
 
-void DFS(int start, int num, int y, int x){
-	if(num == m){
-		if(start == x){
-			cout << start << " => " << y;
+int ladder[32][12];
+int N, M, H;
+
+bool manipulate(){
+	for(int i = 1; i<= N; i++){
+		int ans = i;
+		for(int j = 1; j<= H; j++){
+			if(ladder[j][ans]){
+				ans++;
+			}
+			else if(ladder[j][ans-1]){
+				ans--;
+			}
+		}
+		if(ans != i){
+			return false;
+		}
+	}
+	return true;
+}
+
+
+void DFS(int d, int cnt){
+	if(d == cnt){
+		if(manipulate()){
+			cout << d;
+			exit(0);
 		}
 		
-	}
-	else{
-		for(int i =0; i < 2; i++){
-			int xx = x + dd[i];
-			if(ladder[y][x] == 1 && ladder[y][xx] ==1){
-				DFS(start, num+1, y+1, xx);
+		return;
+	}else{
+		for(int i= 1; i < N; i++){
+			for(int j = 1; j<= H; j++){
+				if(ladder[j][i-1]|| ladder[j][i] || ladder[j][i+1]) continue;
+				else{
+					ladder[j][i] = 1;
+					DFS(d, cnt + 1);
+					ladder[j][i] = 0;
+					
+					while(!ladder[j][i-1] && !ladder[j][i+1]) i++;
+				}	
 			}
 		}
 	}
+	return;
 }
 
 int main(){
 	ios_base::sync_with_stdio(false);
 	freopen("input.txt", "rt", stdin);
-	cin >> n >> m >> h;
+	cin >> N >> M >> H;
+
 	int x, y;
-	for(int i =0; i< m; i++){
+	for(int i =0; i< M; i++){
 		cin >> y >> x;
 		ladder[y][x] = 1;
-		ladder[y][x+1] = 1;
 	}
-	
-	for(int i = 0; i < 4)
-	DFS(1,0,1,1);
+	for(int i =0; i < 4; i++){
+		DFS(i, 0);
+	}	
+	cout << -1;	
 	
 	return 0;
 }
